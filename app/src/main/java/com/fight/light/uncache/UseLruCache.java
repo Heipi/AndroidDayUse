c1bc0aa98179f110e62a73086d548cf4e72f66fb
Hei Pi <kyw0906@163.com>
2018-11-25 00:54:05 +0800

    Build Gradle Commit

--- app/src/main/java/com/fight/light/uncache/UseLruCache.java
+++ app/src/main/java/com/fight/light/uncache/UseLruCache.java
@@ -3,9 +3,7 @@ package com.fight.light.uncache;
 import android.graphics.Bitmap;
 import android.support.v4.util.LruCache;
 
-import com.fight.light.R;
 
-import okhttp3.internal.cache.DiskLruCache;
 
 /**
  * LruCache base implement
